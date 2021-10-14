e5c6cc5bf6c335549570a15e6e720779f40a2a2c
jmjac <jakub@jackowiak.dev>
2021-10-14 03:24:34 -0400

    Return pointers instead of values

--- tradeogre.go
+++ tradeogre.go
@@ -2,8 +2,6 @@ package tradeogreapi
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/url"
 )
@@ -13,9 +11,13 @@ type TradeOgre struct {
 	httpClient *http.Client
 }
 
-func New() (*TradeOgre, error) {
+//New creates new TradeOgre object
+func New(client *http.Client) (*TradeOgre, error) {
+	if client == nil {
+		client = http.DefaultClient
+	}
 	url, err := url.Parse("https://tradeogre.com/api/v1")
-	t := &TradeOgre{url, http.DefaultClient}
+	t := &TradeOgre{url, client}
 	return t, err
 }
 
@@ -31,8 +33,8 @@ func (t *TradeOgre) newRequest(method, path string) (*http.Request, error) {
 	return req, err
 }
 
-// ListMarkets returns tickers and prices for all tickers on tradeogre
-func (t *TradeOgre) ListMarkets() (market, error) {
+// ListMarkets gets a listing of all markets and basic information including current price, volume, high, low, bid and ask
+func (t *TradeOgre) ListMarkets() (*MarketsMap, error) {
 	req, err := t.newRequest("GET", "/markets")
 	if err != nil {
 		return nil, err
@@ -43,78 +45,58 @@ func (t *TradeOgre) ListMarkets() (market, error) {
 		return nil, err
 	}
 
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	var markets market
-	err = json.Unmarshal(body, &markets)
-
-	return markets, err
+	var markets MarketsMap
+	err = json.NewDecoder(resp.Body).Decode(&markets)
+	return &markets, err
 }
 
-func (t *TradeOgre) GetOrderBook(market string) (orderBook, error) {
+// GetOrderBook gets the current order book for market
+func (t *TradeOgre) GetOrderBook(market string) (*OrderBook, error) {
 	req, err := t.newRequest("GET", "/orders/"+market)
 	if err != nil {
-		return orderBook{}, err
+		return nil, err
 	}
 
 	resp, err := t.httpClient.Do(req)
 	if err != nil {
-		return orderBook{}, err
+		return nil, err
 	}
 
-	// Unmarshal the buy and sell to rawMessage that is later converted to string for orderBook
-	var tempBook struct {
-		Success string          `json:"success"`
-		Buy     json.RawMessage `json:"buy"`
-		Sell    json.RawMessage `json:"sell"`
-		this    json.Number     `json:"this"`
-	}
-
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	err = json.Unmarshal(body, &tempBook)
-
-	var ob orderBook
-	ob.Buy = string(tempBook.Buy)
-	ob.Sell = string(tempBook.Sell)
-	ob.Success = tempBook.Success
-
-	return ob, err
+	var orderBook OrderBook
+	err = json.NewDecoder(resp.Body).Decode(&orderBook)
+	return &orderBook, err
 }
 
-func (t *TradeOgre) GetTicker(market string) (ticker, error) {
+// GetTicker gets the ticker for market. Volume, high, and low are in the last 24 hours, initialprice is the price from 24 hours ago.
+func (t *TradeOgre) GetTicker(market string) (*Ticker, error) {
 	req, err := t.newRequest("GET", "/ticker/"+market)
 	if err != nil {
-		return ticker{}, err
+		return nil, err
 	}
 
 	resp, err := t.httpClient.Do(req)
 	if err != nil {
-		return ticker{}, err
+		return nil, err
 	}
 
-	defer resp.Body.Close()
-	var tick ticker
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Printf("body = %+v\n", string(body))
-	err = json.Unmarshal(body, &tick)
-	return tick, err
+	var ticker Ticker
+	err = json.NewDecoder(resp.Body).Decode(&ticker)
+	return &ticker, err
 }
 
-func (t *TradeOgre) GetTradeHistory(market string) (tradeHistory, error) {
+// GetTradeHistory gets the history of the last trades on market limited to 100 of the most recent trades. The date is a Unix UTC timestamp.
+func (t *TradeOgre) GetTradeHistory(market string) (*TradeHistory, error) {
 	req, err := t.newRequest("GET", "/history/"+market)
 	if err != nil {
-		return tradeHistory{}, nil
+		return nil, err
 	}
 
 	resp, err := t.httpClient.Do(req)
 	if err != nil {
-		return tradeHistory{}, nil
+		return nil, err
 	}
 
-	defer resp.Body.Close()
-	var th tradeHistory
-	body, _ := ioutil.ReadAll(resp.Body)
-	err = json.Unmarshal(body, &th)
-	return th, err
+	var tradeHistory TradeHistory
+	err = json.NewDecoder(resp.Body).Decode(&tradeHistory)
+	return &tradeHistory, err
 }
